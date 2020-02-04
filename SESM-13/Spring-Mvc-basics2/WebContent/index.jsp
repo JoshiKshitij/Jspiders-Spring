76973f665198e2b843295c1c9e10028cf8044957
JoshiKshitij <kkshitij.joshi@gmail.com>
2020-02-04 09:34:22 +0530

    form data in method

--- SESM-13/Spring-Mvc-basics2/WebContent/index.jsp
+++ SESM-13/Spring-Mvc-basics2/WebContent/index.jsp
@@ -2,6 +2,14 @@
 <head></head>
 
 <body>
-	<h3>HeLLo TeSt</h3>
+	<h3>Login below !</h3>
+	
+	<form action="/login">
+	<input type="text" name="userName" placeholder="user name">
+	<input type="text" name="password" placeholder="password"><br>
+	<input type="submit" value="login" >
+	</form>
+	
+	
 </body>
 </html>
--- SESM-13/Spring-Mvc-basics2/src/com/test/mvc/WelcomeController.java
+++ SESM-13/Spring-Mvc-basics2/src/com/test/mvc/WelcomeController.java
@@ -1,6 +1,9 @@
 package com.test.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
@@ -8,5 +11,31 @@ public class WelcomeController {
 	public WelcomeController() {
 		System.out.println("Welcome controller created");
 	}
+	
+	
+	@RequestMapping("/welcome")
+	public ModelAndView sayWelcome(){
+		
+		System.out.println("---  Welcome method called ---");
+		
+		return new ModelAndView("index.jsp");
+	}
+	
+	
+	@RequestMapping("/login")
+	public ModelAndView login(
+			@RequestParam String userName,
+			@RequestParam String password){
+		
+		System.out.println("user name - > " +userName);
+		System.out.println("password - > " +password);
+		
+		return new ModelAndView("index.jsp");
+	}
+	
+	
+	
+	
+	
 
 }
